25441b4405554c2bb84cadcd099b6d476f9fdc57
SumitBando-Suraksha <sumit.bando@surakshanet.com>
2024-09-09 22:14:20 +0530

    align healthcheck endpoint to coolify

---
+++ src/routes/healthcheck/+server.ts
@@ -0,0 +1,12 @@
+// src/routes/healthcheck/+server.ts
+
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+    return new Response(JSON.stringify({ status: 'ok' }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
